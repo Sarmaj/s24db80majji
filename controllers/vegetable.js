4c0b3e85806390b5218405602535123e2f3b127d
Sarmaj <156239870+Sarmaj@users.noreply.github.com>
2024-04-05 10:56:48 -0500

    lab12 done

--- controllers/vegetable.js
+++ controllers/vegetable.js
@@ -52,14 +52,14 @@ exports.vegetable_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
-    // Handle Costume create on POST.
+    // Handle vegetable create on POST.
 exports.vegetable_create_post = async function(req, res) {
     console.log(req.body)
     let document = new vegetable();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"vegetable_type":"goat", "cost":12, "size":"large"}
     document.vegetable_type = req.body.vegetable_type;
     document.size = req.body.size;
     document.cost = req.body.cost;
@@ -72,5 +72,35 @@ exports.vegetable_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
-    
+// for a specific vegetable.
+// exports.vegetable_detail = async function(req, res) {
+// console.log("detail" + req.params.id)
+// try {
+// result = await vegetable.findById( req.params.id)
+// res.send(result)
+// } catch (error) {
+// res.status(500)
+// res.send(`{"error": document for id ${req.params.id} not found`);
+// }
+// };
+//
+exports.vegetable_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await vegetable.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.vegetable_type)
+    toUpdate.vegetable_type = req.body.vegetable_type;
+    if(req.body.cost) toUpdate.cost = req.body.cost;
+    if(req.body.size) toUpdate.size = req.body.size;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
     
