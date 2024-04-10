c03bc3dcef86e982479b62a92531b99b4b9006ce
Sarmaj <156239870+Sarmaj@users.noreply.github.com>
2024-04-10 17:21:41 -0500

    9

--- controllers/vegetable.js
+++ controllers/vegetable.js
@@ -73,17 +73,17 @@ exports.vegetable_create_post = async function(req, res) {
     }
     };
 // for a specific vegetable.
-// exports.vegetable_detail = async function(req, res) {
-// console.log("detail" + req.params.id)
-// try {
-// result = await vegetable.findById( req.params.id)
-// res.send(result)
-// } catch (error) {
-// res.status(500)
-// res.send(`{"error": document for id ${req.params.id} not found`);
-// }
-// };
-//
+ exports.vegetable_detail = async function(req, res) {
+ console.log("detail" + req.params.id)
+ try {
+result = await vegetable.findById( req.params.id)
+ res.send(result)
+ } catch (error) {
+ res.status(500)
+ res.send(`{"error": document for id ${req.params.id} not found`);
+ }
+ };
+
 exports.vegetable_update_put = async function(req, res) {
     console.log(`update on id ${req.params.id} with body
     ${JSON.stringify(req.body)}`)
@@ -104,3 +104,88 @@ exports.vegetable_update_put = async function(req, res) {
     }
     };
     
+    // Handle vegetable delete on DELETE.
+exports.vegetable_delete = async function(req, res) {
+console.log("delete " + req.params.id)
+try {
+result = await vegetable.findByIdAndDelete( req.params.id)
+console.log("Removed " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": Error deleting ${err}}`);
+}
+};
+
+// Handle a show one view with id specified by query
+exports.vegetable_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await vegetable.findById( req.query.id)
+    res.render('vegetabledetail',
+    { title: 'vegetable Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle building the view for creating a vegetable.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.vegetable_create_Page = function(req, res) {
+console.log("create view")
+try{
+res.render('vegetablecreate', { title: 'vegetable Create'});
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+
+
+
+    // Handle a show one view with id specified by query
+exports.vegetable_view_one_Page = async function(req, res) {
+console.log("single view for id " + req.query.id)
+try{
+result = await vegetable.findById( req.query.id)
+res.render('vegetabledetail',
+{ title: 'vegetable Detail', toShow: result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+// Handle building the view for updating a vegetable.
+// query provides the id
+exports.vegetable_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await vegetable.findById(req.query.id)
+    res.render('vegetableupdate', { title: 'vegetable Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle a delete one view with id from query
+exports.vegetable_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await vegetable.findById(req.query.id)
+    res.render('vegetabledelete', { title: 'vegetable Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/vegetable.js
+++ routes/vegetable.js
@@ -10,6 +10,10 @@ router.get('/', function(req, res, next) {
   var express = require('express');
   const vegetable_controlers= require('../controllers/vegetable');
   var router = express.Router();
-  /* GET costumes */
+  /* GET vegetables */
   router.get('/', vegetable_controlers.vegetable_view_all_Page);
+  router.get('/detail', vegetable_controlers.vegetable_view_one_Page);
+  router.get('/create', vegetable_controlers.vegetable_create_Page);
+  router.get('/update', vegetable_controlers.vegetable_update_Page);
+  router.get('/delete', vegetable_controlers.vegetable_delete_Page);
   module.exports = router;
